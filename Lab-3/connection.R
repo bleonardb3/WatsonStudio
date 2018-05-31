fb970af1e42aa94552c8c50e9fab05843d260c02
bleonardb3 <bleonardb3@gmail.com>
2018-05-31 11:38:15 -0400

    Add files via upload

---
+++ Lab-3/connection.R
@@ -0,0 +1,12 @@
+# connection.R
+
+# Fill in connection details about your DB2 Warehouse instance below.
+
+dsn.hostname <- 'HOSTNAME HERE'
+dsn.uid <- 'USER ID HERE'
+dsn.pwd <- 'PASSWORD HERE'
+dsn.database <- 'BLUDB'
+dsn.port <- '50000'
+dsn.protocol <- 'TCPIP'
+
+vetting.table = 'VETTING TABLE NAME HERE'
